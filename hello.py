c18272ab93207f8332eebcf20fdb29654589ace7
barbamachado <78858173+barbamachado@users.noreply.github.com>
2023-08-16 08:55:42 -0300

    ^ update headers & script start

--- hello.py
+++ hello.py
@@ -1,4 +1,44 @@
 #!/usr/bin/env python
 
-# imprime hello world
-print("hello world")
+"""hello world multi línguas
+
+escolhe linguagem dependendo da linguagem do ambiente
+
+como usar:
+
+    - configure devidamente a variável LANG, ex:
+        export LANG=pt_BR
+    
+    - execução:
+        python hello.py
+        ou
+        ./hello.py
+"""
+# __*__ => dunder
+__version__ = "0.8.1" 
+__author_ = "lucas machado"
+__license__ = "unlicensed"
+
+# indentifica a variável name e executa caso "main" (feio)
+# if __name__ == "__main__": 
+
+# seta a linguagem atual
+current-language = "en_US"
+
+# armazena texto na var
+msg = "hello world"
+
+# condicional linguagem
+if current-language == "pt_BR":
+    msg = "olá mundo"
+
+
+# imprime a var
+print(msg)
+
+
+
+
+
+
+
