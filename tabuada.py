3becd62c46bbde31b48fcf5ad625bbdd7d865d63
barbamachado <78858173+barbamachado@users.noreply.github.com>
2023-08-21 17:20:59 -0300

    edit tabuada

--- tabuada.py
+++ tabuada.py
@@ -3,7 +3,14 @@
 __version__ = "0.1.0"
 __author__ = "lucas"
 
-# base = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-base = range(1, 11)
+# numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numeros = list(range(1, 11))
 
-print(base)
+# iterables (percorríveis item a item)
+for numero in numeros:
+    print("Tabuada do: ", numero)
+    for outro_numero in numeros:
+        print(outro_numero, "-", numero * outro_numero)
+    print("-----------------")
+
+
